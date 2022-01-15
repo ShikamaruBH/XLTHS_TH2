60749473c19481bc1d56ddbd12308dd8f9caab88
ShikamaruBH <huynhphuquy712001asus4.4.2@gmail.com>
2022-01-15 08:07:58 +0700

    feat(4): Hàm in ma trận nhầm lẫn
    - Thêm / cho nguyên âm đầu hàng

--- pakage/confusionMatrix.py
+++ pakage/confusionMatrix.py
@@ -3,5 +3,5 @@ def confusionMatrix(CONFUSION_MATRIX):
 	vowels = 'aeiou'
 	print(f'{"":5}{"/a/":^5}{"/e/":^5}{"/i/":^5}{"/o/":^5}{"/u/":^5}')
 	for i in range(5):
-		print('{:<5}{:^5.0f}{:^5.0f}{:^5.0f}{:^5.0f}{:^5.0f}'.format(vowels[i], *CONFUSION_MATRIX[i]))
+		print('{:<5}{:^5.0f}{:^5.0f}{:^5.0f}{:^5.0f}{:^5.0f}'.format('/' + vowels[i] + '/', *CONFUSION_MATRIX[i]))
 	print()
