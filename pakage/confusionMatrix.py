aac34671db2a5af0ef0f31841fd5fb2437187d77
ShikamaruBH <74344219+ShikamaruBH@users.noreply.github.com>
2022-01-16 07:55:47 +0700

    feat(4): Hàm in ma trận nhầm lẫn

---
+++ pakage/confusionMatrix.py
@@ -0,0 +1,7 @@
+
+def confusionMatrix(CONFUSION_MATRIX):
+	vowels = 'aeiou'
+	print(f'{"":5}{"/a/":^5}{"/e/":^5}{"/i/":^5}{"/o/":^5}{"/u/":^5}')
+	for i in range(5):
+		print('{:<5}{:^5.0f}{:^5.0f}{:^5.0f}{:^5.0f}{:^5.0f}'.format('/' + vowels[i] + '/', *CONFUSION_MATRIX[i]))
+	print()
