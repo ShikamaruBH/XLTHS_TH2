3d44b3a0154473099a0b74367cea2cc0b734aac8
ShikamaruBH <huynhphuquy712001asus4.4.2@gmail.com>
2022-01-17 07:48:51 +0700

    update(11): Hàm trích xuất vector đường bao phổ của nguyên âm
    - Thêm bước chia đoạn nguyên âm làm 3 phần và lấy đoạn ổn định ở giữa.

--- pakage/getMFCCVectors.py
+++ pakage/getMFCCVectors.py
@@ -2,6 +2,7 @@ from .env import *
 from librosa.feature import mfcc
 
 def getMFCCVectors(vowel_part, Fs, N):
+	vowel_part = vowel_part[int(vowel_part.size / 3):int(vowel_part.size * 2 / 3)]
 	l = []
 	for i in range(0, int(vowel_part.size * Fs), int(FRAME_SHIFT_IN_SECOND * Fs)):
 		if vowel_part[i:i + int(FRAME_LENGHT_IN_SECOND * Fs)].size == int(FRAME_LENGHT_IN_SECOND * Fs):
