6618c98084f1aae0fc952002a17f3b52d86c3491
Hoang Quang Hung <79563371+HunineZ@users.noreply.github.com>
2022-01-20 18:07:16 +0700

    feat(10): Hàm in KQ và tính DCX
    - Xuất bảng kết quả đúng sai nhận dạng
    - Xuất độ chính xác thuật toán
    - Xuất ma trận nhầm lẫn

--- pakage/__init__.py
+++ pakage/__init__.py
@@ -1,6 +1,5 @@
 from .confusionMatrix import confusionMatrix
 from .createDatabase import createDatabase
 from .plotDatabase import plotDatabase
-from .env import *
 from .identificateVowel import identificateVowel
-from .getMFCCVectors import getMFCCVectors
+from .resultTable import resultTable
